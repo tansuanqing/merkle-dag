7f3d40d27077feec0bede8ce3be8be1454b11d08
tansuanqing <132679199+tansuanqing@users.noreply.github.com>
2024-04-07 21:00:42 +0800

    modify my experiment for the 2nd time

--- dag.go
+++ dag.go
@@ -1,41 +1,198 @@
-package merkledag
+package merkledag 
 
 import (
-	"encoding/json"
+	"encoding/json" 
 	"fmt"
-	"strings"
+	"hash" 
+	"math" 
 )
 
-// Hash2File 从 KVStore 中读取与给定哈希相关联的数据，并根据路径返回对应的文件内容
-func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 从 KVStore 中获取与哈希相关联的数据
-	data, err := store.Get(hash)
-	if err != nil {
-		fmt.Println("Error retrieving data from KVStore:", err)
-		return nil
-	}
+const (
+	K = 1 << 10 // K 等于 1 左移 10 位
+	M = K << 10 
+	CHUNK_SIZE = 256 * K 
+	MAX_LISTLINE = 4096 
+	BLOB = "blob" 
+	LIST = "link" 
+	TREE = "tree" 
+)
 
-	// 解码数据，假设数据是 JSON 格式
-	var treeData map[string]interface{}
-	err = json.Unmarshal(data, &treeData)
-	if err != nil {
-		fmt.Println("Error decoding data:", err)
-		return nil
-	}
-
-	// 根据路径递归查找文件内容
-	parts := strings.Split(path, "/") //使用 "/" 分割路径
-	currentData := treeData
-	for _, part := range parts {
-		if part == "" {
-			continue
-		}
-		if node, ok := currentData[part].(map[string]interface{}); ok { // 如果当前部分是 map 类型
-			currentData = node
-		} else {
-			// 文件内容为叶子节点的值
-			return []byte(fmt.Sprintf("%v", currentData[part]))
-		}
-	}
-	return nil
+type Link struct {
+	Name string 
+	Hash []byte 
+	Size int 
 }
+
+type Object struct { 
+	Links []Link 
+	Data  []byte 
+
+// Add 函数将分片写入到 KVStore 中，并返回 Merkle Root
+func Add(store KVStore, node Node, h hash.Hash) []byte { // Add 函数，接收 KVStore、Node 和 hash.Hash 参数，返回字节切片
+	obj := &Object{} // 创建 Object 对象
+	switch node.Type() {
+	case FILE: // 如果是文件类型
+		obj = handleFile(node, store, h) 
+		break
+	case DIR: // 文件夹类型
+		obj = handleDir(node, store, h) 
+		break
+	}
+	jsonObj, _ := json.Marshal(obj) 
+	return computeHash(jsonObj, h) 
+}
+
+// 处理文件，返回一个该文件对应的 obj
+func handleFile(node Node, store KVStore, h hash.Hash) *Object { 
+	obj := &Object{} 
+	FileNode, _ := node.(File) 
+	if FileNode.Size() > CHUNK_SIZE { 
+		numChunks := math.Ceil(float64(FileNode.Size()) / float64(CHUNK_SIZE)) // 计算分块数量
+		height := 0
+		tmp := numChunks
+		// 计算出要分几层
+		for {
+			height++
+			tmp /= MAX_LISTLINE
+			if tmp == 0 {
+				break
+			}
+		}
+		obj, _ = dfshandleFile(height, FileNode, store, 0, h) // 递归处理大文件
+	} else {
+		obj.Data = FileNode.Bytes() // 将文件内容存入 Data
+		putObjInStore(obj, store, h, BLOB) // 将对象存入 KVStore
+	}
+	return obj // 返回对象
+}
+
+// 处理文件夹，返回对应的 obj 指针
+func handleDir(node Node, store KVStore, h hash.Hash) *Object { // 处理文件夹函数，接收节点、KVStore 和 hash.Hash 参数，返回 Object 指针
+	dirNode, _ := node.(Dir) 
+	iter := dirNode.It() 
+treeObject := &Object{} 
+	for iter.Next() { 
+		node := iter.Node()
+		switch node.Type() { 
+		case FILE: 
+			file := node.(File) 
+			tmp := handleFile(node, store, h) 
+			jsonMarshal, _ := json.Marshal(tmp) 
+			treeObject.Links = append(treeObject.Links, Link{ 
+				Hash: computeHash(jsonMarshal, h),
+				Size: int(file.Size()), 
+				Name: file.Name(), 
+			})
+			if tmp.Links == nil {
+				treeObject.Data = append(treeObject.Data, []byte(BLOB)...) 
+			} else {
+				treeObject.Data = append(treeObject.Data, []byte(LIST)...) 
+			}
+			break
+		case DIR:
+			dir := node.(Dir) 
+			tmp := handleDir(node, store, h) 
+			jsonMarshal, _ := json.Marshal(tmp) 
+			treeObject.Links = append(treeObject.Links, Link{ 
+				Hash: computeHash(jsonMarshal, h),
+				Size: int(dir.Size()), 
+				Name: dir.Name(), 
+			})
+			treeObject.Data = append(treeObject.Data, []byte(TREE)...) 
+			break
+		}
+	}
+	putObjInStore(treeObject, store, h, LIST) 
+	return treeObject 
+}
+
+// 处理大文件的方法，递归调用，返回当前生成的 obj 已经处理了多少数据
+func dfshandleFile(height int, node File, store KVStore, start int, h hash.Hash) (*Object, int) { 
+	obj := &Object{} 
+	lendata := 0 // 处理数据量
+	if height == 1 { // 如果只有一层
+		if len(node.Bytes())-start < CHUNK_SIZE { 
+			data := node.Bytes()[start:] // 取剩余数据
+			obj.Data = append(obj.Data, data...) 
+			lendata = len(data) 
+			putObjInStore(obj, store, h, BLOB) 
+			return obj, lendata 
+		} else { 
+			for i := 1; i <= MAX_LISTLINE; i++ {
+				end := start + CHUNK_SIZE 
+				// 确保不越界
+				if end > len(node.Bytes()) { 
+					end = len(node.Bytes()) 
+				}
+				data := node.Bytes()[start:end] 
+				blobObj := Object{
+					Data:  data,
+				}
+				putObjInStore(&blobObj, store, h, BLOB) 
+				jsonMarshal, _ := json.Marshal(blobObj) 
+				obj.Links = append(obj.Links, Link{ 
+					Hash: computeHash(jsonMarshal, h), 
+					Size: int(len(data)), 
+				})
+				obj.Data = append(obj.Data, []byte(BLOB)...) 
+				lendata += len(data) 
+				start += CHUNK_SIZE 
+				if start >= len(node.Bytes()) {
+					break 
+				}
+			}
+			putObjInStore(obj, store, h, LIST) 
+			return obj, lendata 
+		}
+	} else {
+		for i := 1; i <= MAX_LISTLINE; i++ { 
+			if start >= len(node.Bytes()) { 
+				break 
+			tmpObj, tmpLendata := dfshandleFile(height-1, node, store, start, h) 
+			lendata += tmpLendata 
+			jsonMarshal, _ := json.Marshal(tmpObj) 
+			obj.Links = append(obj.Links, Link{ 
+				Hash: computeHash(jsonMarshal, h), 
+				Size: tmpLendata, 
+			})
+			if tmpObj.Links == nil { 
+				obj.Data = append(obj.Data, []byte(BLOB)...) 
+			} else {
+				obj.Data = append(obj.Data, []byte(LIST)...) 
+			}
+			start += tmpLendata 
+		}
+		putObjInStore(obj, store, h, LIST) 
+		return obj, lendata 
+	}
+}
+
+func computeHash(data []byte, h hash.Hash) []byte { 
+	h.Reset() 
+	h.Write(data) 
+	return h.Sum(nil) 
+}
+
+func putObjInStore(obj *Object, store KVStore, h hash.Hash, objType string) { 
+	value, err := json.Marshal(obj) 
+	if err != nil { 
+		fmt.Println("json.Marshal err:", err) 
+		return 
+	}
+
+	hash := computeHash(value, h) 
+	flag, _ := store.Has(hash) 
+	if flag {
+		return 
+	}
+	if objType == BLOB { 
+		store.Put(hash, obj.Data) 
+	} else { 
+		store.Put(hash, value) 
+	}
+
+}
+
+
+
+
--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,75 @@
-package merkledag
+package merkledag 
+import (
+	"encoding/json" 
+	"strings" 
+)
 
-// Hash to file
-func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
-	return nil
+func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte { // Hash2File 函数，根据哈希和路径返回文件内容
+	// 根据 hash 和 path， 返回对应的文件, hash 对应的类型是 tree
+	flag, _ := store.Has(hash) // 检查哈希是否存在
+	if flag { 
+		objBinary, _ := store.Get(hash) 
+		var obj Object
+		json.Unmarshal(objBinary, &obj) // 反序列化对象
+		pathArr := strings.Split(path, "/") // 按 "/" 分割路径
+		cur := 1
+		return getFileByDir(obj, pathArr, cur, store) // 根据路径获取文件内容
+	}
+	return nil 
 }
+
+func getFileByDir(obj Object, pathArr []string, cur int, store KVStore) []byte { // 根据文件夹对象和路径获取文件内容
+	if cur >= len(pathArr) { // 如果当前位置超出路径长度
+		return nil 
+	}
+	index := 0
+	for i := range obj.Links { // 遍历链接列表
+		objType := string(obj.Data[index : index+4]) // 类型
+		index += 4
+		objInfo := obj.Links[i] // 信息
+		if objInfo.Name != pathArr[cur] { // 名称路径当前位置不匹配
+			continue 
+		}
+		switch objType {
+		case TREE: 
+			objDirBinary, _ := store.Get(objInfo.Hash) // 获取文件夹对象的二进制数据
+			var objDir Object 
+			json.Unmarshal(objDirBinary, &objDir) 
+			ans := getFileByDir(objDir, pathArr, cur+1, store)
+			if ans != nil { 
+				return ans 
+			}
+		case BLOB: // 二进制文件
+			ans, _ := store.Get(objInfo.Hash) 
+			return ans 
+		case LIST: 
+			objLinkBinary, _ := store.Get(objInfo.Hash) // 获取列表对象的二进制数据
+			var objLink Object 
+			json.Unmarshal(objLinkBinary, &objLink) 
+			ans := getFileByList(objLink, store) 
+			return ans 
+		}
+	}
+	return nil 
+}
+
+func getFileByList(obj Object, store KVStore) []byte { // 根据列表对象获取文件内容
+	ans := make([]byte, 0) // 初始化结果字节切片
+	index := 0
+	for i := range obj.Links { // 遍历列表对象的链接列表
+		curObjType := string(obj.Data[index : index+4]) 
+		index += 4
+		curObjLink := obj.Links[i] 
+		curObjBinary, _ := store.Get(curObjLink.Hash) 
+		var curObj Object 
+		json.Unmarshal(curObjBinary, &curObj) 
+		if curObjType == BLOB { 
+			ans = append(ans, curObjBinary...) 
+		} else { 
+			tmp := getFileByList(curObj, store) 
+			ans = append(ans, tmp...) 
+		}
+	}
+	return ans 
+
+
